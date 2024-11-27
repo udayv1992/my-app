340663a9276a6befddb9203f60bd6495818686c3
udayv1992 <pawaruday096@gmail.com>
2024-11-27 10:29:40 +0530

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  public loginForm:FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  })
+  login(){
+    console.log(this.loginForm)
+  }
 }
